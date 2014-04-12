f63780109e88acfb6cc1b10726e72414188968af
Douglass Chen <douglasschen@gmail.com>
2014-04-12 11:35:25 -0700

    once again, resync'd the metronome and song

--- src/com/example/unnamedrhythmgameblindaccessible/GameActivity.java
+++ src/com/example/unnamedrhythmgameblindaccessible/GameActivity.java
@@ -9,6 +9,7 @@ import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.media.MediaPlayer;
+import android.media.MediaPlayer.OnCompletionListener;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -16,7 +17,7 @@ import android.widget.Button;
 
 public class GameActivity extends Activity implements SensorEventListener {
 
-	final long START_DELAY = 5000; // delay before ANY playing happens
+	final long START_DELAY = 4000; // delay before ANY playing happens
     final int MAX_RECORDS = 200;
     final float SHAKE_THRESHOLD = 9.5f; // min z-max/min difference 
     
@@ -29,18 +30,19 @@ public class GameActivity extends Activity implements SensorEventListener {
     
     // do I need these? more to come
     // TODO: hardcoded for Get Lucky
-    long SONG_LENGTH_MS = 408413;
-    long SONG_DELAY_MS = 140;
-    double SONG_BPM = 116.05;
-    long NUMBER_BEATS;
+    double SONG_LENGTH_MS;
+    double SONG_DELAY_MS = 140.0;
+    double SONG_BPM = 116.050 / 2;
+    double NUMBER_BEATS;
     double BEAT_LENGTH;
 	
     // UI
 	Button tapButton;
 	
 	// sound stuff
-	SoundManager mSoundManager;
-	MediaPlayer mMediaPlayer;
+	// SoundManager mSoundManager;
+	MediaPlayer mMediaPlayerSong;
+	MediaPlayer mMediaPlayerMetronome;
 	
 	// sensor stuff
     SensorManager mSensorManager;
@@ -49,6 +51,7 @@ public class GameActivity extends Activity implements SensorEventListener {
     // timer stuff
     Timer mainTimer = new Timer();
     TimerTask beginNextInterval;
+    TimerTask endNextInterval;
     TimerTask beginSong;
 	
 	@Override
@@ -58,33 +61,52 @@ public class GameActivity extends Activity implements SensorEventListener {
 		
 		tapButton = (Button)findViewById(R.id.tap_button);
 
-        NUMBER_BEATS = (long)((SONG_BPM / 60.0) * (SONG_LENGTH_MS / 1000.0));
+
+		SONG_LENGTH_MS = MediaPlayer.create(getApplicationContext(), R.raw.daftpunk).getDuration();
+        NUMBER_BEATS = ((SONG_BPM / 60.0) * (SONG_LENGTH_MS / 1000.0));
         BEAT_LENGTH = SONG_LENGTH_MS / NUMBER_BEATS;
         Log.d("GameActivity", "NUMBER_BEATS, BEAT_LENGTH = " + NUMBER_BEATS + " " + BEAT_LENGTH);
         
 		mSensorManager = (SensorManager)getSystemService(SENSOR_SERVICE);
         mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        
+        /*
         mSoundManager = new SoundManager();
         mSoundManager.initSounds(getBaseContext());
         
         // sound effects
-        mSoundManager.addSound(1, R.raw.whistle);
+        mSoundManager.addSound(1, R.raw.clap);
         
         // music
         mSoundManager.addSound(10, R.raw.daftpunk);
+        */
 	}
 
 	@Override
 	protected void onResume() {
 		super.onResume();
 
-        mMediaPlayer = MediaPlayer.create(GameActivity.this, R.raw.daftpunk);
+        mMediaPlayerSong = MediaPlayer.create(GameActivity.this, R.raw.daftpunk);
 		// timer tasks defined: check if shake or tap done
         beginNextInterval = new TimerTask() {
 			public void run()
 			{
+				if(mMediaPlayerMetronome != null) 
+				{
+					mMediaPlayerMetronome.stop();
+					mMediaPlayerMetronome.release();
+				}
 				// Log.d("GameActivity", "playing metronome sound");
-				mSoundManager.playSound(1);
+				// mSoundManager.playSound(1);
+		        mMediaPlayerMetronome = MediaPlayer.create(GameActivity.this, R.raw.clap);
+				mMediaPlayerMetronome.start();
+				/*
+				mMediaPlayerMetronome.setOnCompletionListener(new OnCompletionListener() {
+			        public void onCompletion(MediaPlayer mp) {
+			        	mMediaPlayerMetronome.release();
+			        };
+			    });
+			    */
 				if(mAccelPeak > SHAKE_THRESHOLD)
 				{
 					Log.d("GameActivity", "Shake threshold reached");
@@ -93,16 +115,23 @@ public class GameActivity extends Activity implements SensorEventListener {
 				}
 			}
         };
+        endNextInterval = new TimerTask() {
+			public void run()
+			{
+				mMediaPlayerMetronome.stop();
+				mMediaPlayerMetronome.release();
+			}
+        };
         beginSong = new TimerTask() {
 			public void run()
 			{
-				mMediaPlayer.start(); // play the song!
+				mMediaPlayerSong.start(); // play the song!
 			}
         };
 		
         // initialize gestureTimer and the sensor manager on starting or resuming app
 		mainTimer = new Timer();
-		mainTimer.scheduleAtFixedRate(beginNextInterval, START_DELAY + SONG_DELAY_MS, (long)BEAT_LENGTH);
+		mainTimer.scheduleAtFixedRate(beginNextInterval, (long)(START_DELAY + SONG_DELAY_MS), (long)BEAT_LENGTH);
 		mainTimer.schedule(beginSong, START_DELAY);
 		mSensorManager.registerListener(this, mAccelerometer, SensorManager.SENSOR_DELAY_GAME);
 	}
@@ -112,8 +141,8 @@ public class GameActivity extends Activity implements SensorEventListener {
 		super.onPause();
 		mainTimer.cancel();
 		mSensorManager.unregisterListener(this);
-		mMediaPlayer.stop();
-		mMediaPlayer.reset();
+		mMediaPlayerSong.stop();
+		mMediaPlayerSong.reset();
 	}
 	
 	public void onSensorChanged(SensorEvent event) {
